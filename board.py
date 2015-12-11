99606b462459a6ff5485926176a8c9cd43ff3654
hkwu <kelvinwu1996@gmail.com>
2015-12-10 20:38:54 -0500

    Initial commit.

---
+++ board.py
@@ -0,0 +1,15 @@
+#################
+# Monopoly/Engine
+# board.py
+# Kelvin Wu
+#################
+
+class Board(object):
+    """Handles state information for the board."""
+    def __init__(self):
+        super().__init__()
+        self.tiles = []
+
+    def init_tiles(self):
+        """Reads in tiles from the specified data file."""
+        pass
